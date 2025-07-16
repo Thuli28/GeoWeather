e96709bf921d9915929576cdf2f56f2fd5997089
Malethola Raditsela <raditselamalethola92@gmail.com>
2025-07-16 13:13:29 +0200

    addede ID for temperature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+function newWeather(response){
+    let temperature = document.querySelector
+}
 function searchEngine(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = searchInput.value;
+
 }
 let searchformElement = document.querySelector("#search-form");
 searchformElement.addEventListener("submit", searchEngine);
