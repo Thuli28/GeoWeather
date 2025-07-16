16f54def23a7df3d7761fc69585571bc487911ac
Malethola Raditsela <raditselamalethola92@gmail.com>
2025-07-16 14:11:40 +0200

    Added basic API  for temperature

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-function newWeather(response){
-    let temperature = document.querySelector
+function newWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let city = document.querySelector("#current-city");
+  city.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "5261f4be982ete699385co0647b7a120";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(newWeather);
 }
 function searchEngine(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = searchInput.value;
-
+  searchCity(searchInput.value);
 }
 let searchformElement = document.querySelector("#search-form");
 searchformElement.addEventListener("submit", searchEngine);
